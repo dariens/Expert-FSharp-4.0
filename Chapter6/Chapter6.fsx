4f0daf2049692c82218103db46a35a514e8ddd48
dariens <darien.shannon@gmail.com>
2017-04-23 23:22:31 -0700

    completed chap 6

--- Chapter6/Chapter6.fsx
+++ Chapter6/Chapter6.fsx
@@ -27,16 +27,16 @@ type Vector2DOld =
 
 let v = {DX = 3.0; DY = 4.0}
 
-v.Length
+//v.Length
 
 
-v.Scale(2.0).Length
+//v.Scale(2.0).Length
 
-Vector2DOld.ConstX(3.0)
+//Vector2DOld.ConstX(3.0)
 
 let newVector = {DX = 4.0; DY = 4.0}
 
-newVector.LengthWithSideEffect
+//newVector.LengthWithSideEffect
 
 /// A type of binary tree, generic in the type of values carried at nodes
 type Tree<'T> =
@@ -68,8 +68,8 @@ type Vector2D(dx : float, dy : float) =
 let v2= Vector2D(3.0, 4.0)
 let v3 = Vector2D.OneX
 
-v2.Length
-v2.Scale(2.0).Length
+//v2.Length
+//v2.Scale(2.0).Length
 
 /// Vectors whose length is checked to be close to length one.
 type UnitVector2D(dx,dy) =
@@ -100,7 +100,7 @@ type SparseVector(items : seq<int * float>) =
 
 let v5 = SparseVector [|(3, 547.0); (4, 550.0)|]
 
-v5.[3]
+//v5.[3]
 
 ///Adding Overloaded Operators
 
@@ -117,15 +117,15 @@ type Vector2DWithOperators(dx : float, dy : float) =
     member v.Print () = printfn "DX = %f; DY = %f" v.DX v.DY
 
 let v6 = new Vector2DWithOperators(3.0, 4.0)
-v6.Print ()
+//v6.Print ()
 let v7 = v6 + v6
-v7.Print ()
+//v7.Print ()
 
 let (++) x y = List.append x [y]
 
 let list = [1;2;3]
 
-list ++ 4
+//list ++ 4
 
 /// Using Named and Optional Arguments
 
@@ -141,11 +141,11 @@ type LabelInfo(?text : string, ?font : Font) =
 
     static member Create(?text, ?font) = new LabelInfo(?text = text, ?font = font)
 
-LabelInfo (text = "Hello World")
-LabelInfo("Goodbye Lenin")
-LabelInfo.Create()
-LabelInfo(font = new Font(FontFamily.GenericMonospace, 36.0f),
-          text = "Imagine")
+//LabelInfo (text = "Hello World")
+//LabelInfo("Goodbye Lenin")
+//LabelInfo.Create()
+//LabelInfo(font = new Font(FontFamily.GenericMonospace, 36.0f),
+//          text = "Imagine")
 
 
 /// Adding Method Overloading
@@ -175,8 +175,8 @@ let interval1 = Interval(3.0, 6.0)
 let interval2 = Interval(5.0, 9.0)
 let interval3 = Interval (12.0, 14.0)
 let intervalSeq = seq [interval1;interval2;interval3]
-Interval.Span(interval1,interval2)
-Interval.Span(intervalSeq)
+//Interval.Span(interval1,interval2)
+//Interval.Span(intervalSeq)
 
 type Vector =
     { DX : float; DY : float}
@@ -216,13 +216,13 @@ type MutableVector2D(dx : float, dy : float) =
 
 let myVector = MutableVector2D(3.0, 4.0)
 
-(myVector.DX, myVector.DY)
+//(myVector.DX, myVector.DY)
 
-(myVector.Length, myVector.Angle)
+//(myVector.Length, myVector.Angle)
 
-myVector.Angle <- System.Math.PI / 6.0
+//myVector.Angle <- System.Math.PI / 6.0
 
-(myVector.DX, myVector.DY, myVector.Length)
+//(myVector.DX, myVector.DY, myVector.Length)
 
 
 open System.Collections.Generic
@@ -235,10 +235,10 @@ type IntegerMatrix(rows : int, cols : int) =
 
 let myMatrix = IntegerMatrix(10, 10)
 
-myMatrix.[5, 5] <- (Some 10)
+//myMatrix.[5, 5] <- (Some 10)
 
-myMatrix.[5,5]
-myMatrix.[9,9]
+//myMatrix.[5,5]
+//myMatrix.[9,9]
 
 /// Using Optional Property Settings
 
@@ -257,7 +257,7 @@ type LabelInfoWithPropertySetting() =
 
 let labelInfo = LabelInfoWithPropertySetting(Text = "Hello World")
 
-let form = new Form(Visible = true, TopMost = true, Text = "Welcome to F#")
+//let form = new Form(Visible = true, TopMost = true, Text = "Welcome to F#")
 
 /// Getting Started with Object Interface Types
 
@@ -311,13 +311,13 @@ let square (center : PointF, side : float32) =
             sprintf "Center: (%f, %f), Side: %f" center.X center.Y side}
 
 let bigCircle = circle(PointF(0.0f, 0.0f), 100.0f)
-bigCircle.BoundingBox
-bigCircle.Contains(PointF(70.0f,70.0f))
-bigCircle.Contains(PointF(71.0f,71.0f))
+//bigCircle.BoundingBox
+//bigCircle.Contains(PointF(70.0f,70.0f))
+//bigCircle.Contains(PointF(71.0f,71.0f))
 
 let smallSquare = square(PointF(1.0f, 1.0f), 1.0f)
-smallSquare.BoundingBox
-smallSquare.Contains(PointF(0.0f, 0.0f))
+//smallSquare.BoundingBox
+//smallSquare.Contains(PointF(0.0f, 0.0f))
 
 /// IMplementing Object Interface Types Using Concrete Types
 
@@ -366,8 +366,8 @@ type MutableCircle(center: PointF, radius: float32) =
 
 
 let circle2 = MutableCircle()
-circle2.Radius
-(circle2 :> IShape).BoundingBox
+//circle2.Radius
+//(circle2 :> IShape).BoundingBox
 
 let r1 = square(new PointF(1.0f, 1.0f), 2.0f) 
 let c1 = new MutableCircle() :> IShape
@@ -377,7 +377,7 @@ let c4 = new MutableCircle(radius = 4.0f) :> IShape
 
 let printShapeInfo (c:IShape) = printfn "%s" c.ShapeInfo
 
-[r1;c1;c2;c3;c4] |> List.map printShapeInfo
+//[r1;c1;c2;c3;c4] |> List.map printShapeInfo
 
 ///Using Common Object Interface Types from the .Net Libraries
 
@@ -427,12 +427,12 @@ open System.Text
 let buf = new StringBuilder()
 let c = stringBuilderOutputSink(buf)
 
-buf.ToString()
-["Incy"; " "; "Wincy"; " "; "Spider"] |> List.iter c.WriteString
-buf.ToString()
+//buf.ToString()
+//["Incy"; " "; "Wincy"; " "; "Spider"] |> List.iter c.WriteString
+//buf.ToString()
 
-[';';' '; 'D';'o';'n';'e'] |> List.iter c.WriteChar
-buf.ToString()
+//[';';' '; 'D';'o';'n';'e'] |> List.iter c.WriteChar
+//buf.ToString()
 
 /// A type that fully implements the ITextOutputSink object interface
 type CountingOutputSink(writeCharFunction : char -> unit) =
@@ -443,3 +443,366 @@ type CountingOutputSink(writeCharFunction : char -> unit) =
     member x.Count = count
 
 /// Defining Partially Implement Class Types
+
+[<AbstractClass>]
+type TextOutputSink() =
+    abstract WriteChar : char -> unit
+    abstract WriteString : string -> unit
+    default x.WriteString s = s |> String.iter x.WriteChar
+
+/// Using Partially Implemented Types via Delegation
+
+/// A type which usees a TextOutputSink internally
+type HtmlWriter() =
+    let mutable count = 0
+    let sink =
+        { new TextOutputSink() with
+              member x.WriteChar c =
+                  count <- count + 1;
+                  System.Console.Write c}
+
+    member x.CharCount = count
+    member x.OpenTag(tagName) = sink.WriteString(sprintf "<%s>" tagName)
+    member x.CloseTag(tagName) = sink.WriteString(sprintf "</%s>" tagName)
+    member x.WriteString(s) = sink.WriteString(s)
+
+/// Using Partially IMplemented Types via Implementation Inheritance
+
+/// An implementation of TextOutputSink, counting the number of bytes written
+type CountingOutputSinkByInheritance() =
+    inherit TextOutputSink()
+
+    let mutable count = 0
+
+    member sink.Count = count
+
+    default sink.WriteChar c =
+        count <- count + 1
+        System.Console.Write c
+
+open System.Text
+/// A component to write bytes to an ouput sink
+[<AbstractClass>]
+type ByteOutputSink() =
+    inherit TextOutputSink()
+
+    /// When implemented, writes one byte to the sink
+    abstract WriteByte : byte -> unit
+    
+    /// When implemented, writes multiple bytes to the sink
+    abstract WriteBytes : byte[] -> unit
+
+    default sink.WriteChar c = sink.WriteBytes(Encoding.UTF8.GetBytes [|c|])
+
+    override sink.WriteString s = sink.WriteBytes(Encoding.UTF8.GetBytes s)
+    
+    default sink.WriteBytes b = b |> Array.iter sink.WriteByte
+
+/// Combining Functionl and Objects: Cleaning Up Resources
+open System.IO
+let myWriteStringToFile1() =
+    use outp = File.CreateText("playlist.txt")
+    outp.WriteLine("Enchanted")
+    outp.WriteLine("Put your records on")
+
+// This is eqivalent ot the following:
+let myWriteStringToFile () =
+    let outp = File.CreateText("playlist.txt")
+    try
+        outp.WriteLine("Enchanted")
+        outp.WriteLine("Put your records on")
+    finally
+        (outp :> System.IDisposable).Dispose()
+
+/// Resources and IDisposable
+
+let http (url : string) =
+   let req = System.Net.WebRequest.Create url
+   use resp = req.GetResponse()
+   use stream = resp.GetResponseStream()
+   use reader = new System.IO.StreamReader(stream)
+   let html = reader.ReadToEnd()
+   html
+
+/// Managing Resoruces with More-Complex Liftimes
+
+/// Cleaning Up Internal Objects
+
+// Implementing IDisposable to clean up internal objects
+
+open System.IO
+
+type LineChooser(fileName1, fileName2) =
+    let file1 = File.OpenText(fileName1)
+    let file2 = File.OpenText(fileName2)
+    let rnd = new System.Random()
+
+    let mutable disposed = false
+
+    let cleanup() =
+        if not disposed then
+            disposed <- true;
+            file1.Dispose();
+            file2.Dispose();
+    
+    interface System.IDisposable with
+        member x.Dispose() = cleanup()
+
+    member obj.CloseAll() = cleanup()
+
+    member obj.GetLine() =
+        if not file1.EndOfStream &&
+            (file2.EndOfStream || rnd.Next() % 2 = 0) then file1.ReadLine()
+        elif not file2.EndOfStream then file2.ReadLine()
+        else raise (new EndOfStreamException())
+
+open System
+
+open System.IO
+//File.WriteAllLines("test1.txt", [|"Daisy, Daisy"; "Give me your hand oh do"|])
+//File.WriteAllLines("test2.txt", [|"I'm a little teapot"; "Short and stout"|])
+let chooser = new LineChooser("test1.txt", "test2.txt")
+
+//chooser.GetLine()
+//chooser.GetLine()
+//(chooser :> IDisposable).Dispose()
+//chooser.GetLine()
+
+/// Cleaning Up Unmangaed Objects
+
+/// Reclaiming unmanaged tickets with IDisposable
+
+open System
+
+type TicketGenerator() =
+    let mutable free = []
+    let mutable max = 0
+
+    member h.Alloc() =
+        match free with
+        | [] -> max <- max + 1; max
+        | h :: t -> free <- t; h
+
+    member h.Dealloc(n:int) =
+        printfn "returning ticket %d" n
+        free <- n :: free
+
+let ticketGenerator = new TicketGenerator()
+
+type Customer() =
+    let myTicket = ticketGenerator.Alloc()
+    let mutable disposed = false
+    let cleanup() =
+        if not disposed then
+            disposed <-true
+            ticketGenerator.Dealloc(myTicket)
+
+    member x.Ticket = myTicket
+
+    override x.Finalize() = cleanup()
+
+    interface IDisposable with
+        member x.Dispose() = cleanup(); GC.SuppressFinalize(x)
+
+let bill = new Customer()
+
+//bill.Ticket
+
+
+//(use joe = new Customer() in printfn "joe.Ticket = %d" joe.Ticket)
+
+//(use jane = new Customer() in printfn "jane.Ticket = %d" jane.Ticket)
+
+let darien = new Customer()
+
+//(use darina = new Customer() in printfn "darina.Ticket = %d" darina.Ticket)
+
+//(bill :> IDisposable).Dispose()
+
+//(use darina = new Customer() in printfn "darina.Ticket = %d" darina.Ticket)
+
+let darina = new Customer()
+//darina.Ticket
+
+/// Extending Existing Types and Modules
+
+module NumberTheoryExtensions =
+    let factorize i =
+        let lim = int (sqrt (float i))
+        let rec check j =
+            if j > lim then None
+            elif (i % j) = 0 then Some (i / j, j)
+            else check (j + 1)
+        check 2
+
+    type System.Int32 with
+        member i.IsPrime = (factorize i).IsNone
+        member i.TryFactorize() = factorize i
+
+open NumberTheoryExtensions
+
+//(2 + 1).IsPrime
+
+//(6093704 + 11).TryFactorize()
+
+module CSharpStyleExtensions =
+    
+    open System.Runtime.CompilerServices
+
+    let factorize i =
+        let lim = int (sqrt (float i))
+        let rec check j =
+            if j > lim then None
+            elif (i % j) = 0 then Some (i / j, j)
+            else check (j + 1)
+        check 2
+
+    [<Extension>]
+    type Int32Extensions() =
+        [<Extension>]
+        static member IsPrime2(i:int) = (factorize i).IsNone
+
+        [<Extension>]
+        static member TryFactorize2(i:int) = factorize i
+
+    [<Extension>]
+    type ResizeArrayExtensions() =
+        [<Extension>]
+        static member Product(values:ResizeArray<int>) =
+            let mutable total = 1
+            for v in values do
+                total <- total * v
+            total
+
+        [<Extension>]
+        static member inline GenericProduct(values:ResizeArray<'T>) =
+            let mutable total = LanguagePrimitives.GenericOne<'T>
+            for v in values do
+                total <- total * v
+            total
+
+
+open CSharpStyleExtensions
+
+//(2 + 1).IsPrime2()
+
+//(6093704 + 11).TryFactorize2()
+
+open System.Collections.Generic
+
+let arr = ResizeArray([1 .. 10])
+
+let arr2 = ResizeArray([ 1L .. 10L ])
+
+//arr.Product()
+
+//arr.GenericProduct()
+
+//arr2.GenericProduct()
+
+module List =
+    let rec pairwise l =
+        match l with
+        | [] | [_] -> []
+        | h1 :: ((h2 :: _) as t) -> (h1, h2) :: pairwise t
+
+//List.pairwise [1; 2; 3; 4]
+
+/// Working with F# Objects and .Net Types
+
+type Vector2D2(dx : float, dy : float) =
+    class
+        let len = sqrt(dx * dx + dy * dy)
+        member v.DX = dx
+        member v.DY = dy
+        member v.Length = len
+    end
+
+[<Class>]
+type Vector2D3(dx : float, dy : float) =
+    let len = sqrt(dx * dx + dy * dy)
+    member v.DX = dx
+    member v.DY = dy
+    member v.Length = len
+
+type IShape2 =
+    interface
+        abstract Contains : Point -> bool
+        abstract BoundingBox : Rectangle
+    end
+
+[<Interface>]
+type IShape3 =
+    abstract Contains : Point -> bool
+    abstract BoundingBox : Rectangle
+
+/// Structs
+
+[<Struct>]
+type Vector2DStruct(dx : float, dy : float) =
+    member v.DX = dx
+    member v.DY = dy
+    member v.Length = sqrt (dx * dx + dy * dy)
+
+[<Struct>]
+type Vector2DStructUsingExplicitVals =
+    val dx : float
+    val dy : float
+    member v.DX = v.dx
+    member v.DY = v.dy
+    member v.Length = sqrt (v.dx * v.dx + v.dy * v.dy)
+
+/// Delegates
+
+type ControlEventHandler = delegate of int -> bool
+
+///Add a new handler ot the Win32 ctrl + c handling API
+open System.Runtime.InteropServices
+
+let ctrlSignal = ref false
+
+[<DllImport("kernel32.dll")>]
+extern void SetConsoleCtrlHandler(ControlEventHandler callback, bool add)
+
+let ctrlEventHandler = new ControlEventHandler(fun i -> ctrlSignal := true; true)
+
+//SetConsoleCtrlHandler(ctrlEventHandler, true)
+
+
+/// Enums
+
+type Vowels =
+    | A = 1
+    | E = 5
+    | I = 9
+    | O = 15
+    | U = 21
+
+/// Working with null Values
+
+let parents = [("Adam", None); ("Cain", Some("Adam", "Eve"))]
+
+//match System.Environment.GetEnvironmentVariable("PATH") with
+//| null -> printf "the enviornment variable PATH is not defined\n"
+//| res -> printf "the enviornment variable PATH is set to %s\n" res
+
+let switchOnType (a : obj) =
+    match a with
+    | null -> printf "null!"
+    | :? System.Exception as e -> printf "An exception: %s!" e.Message
+    | :? System.Int32 as i -> printf "An integer: %d!" i
+    | :? System.DateTime as d -> printf "A date/time: %O!" d
+    | _ -> printf "Some other kind of object\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
