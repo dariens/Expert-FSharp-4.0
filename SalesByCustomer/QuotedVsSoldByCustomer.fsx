7e60f7e2bcb6624e338495eaf9af0bb3f57a0018
dariens <darien.shannon@gmail.com>
2017-05-01 08:40:47 -0700

    Hope I didnt screw things up

--- SalesByCustomer/QuotedVsSoldByCustomer.fsx
+++ SalesByCustomer/QuotedVsSoldByCustomer.fsx
@@ -8,31 +8,38 @@ let dataPath = scriptLocation + @"\Data"
 let startDate = System.DateTime(2016, 1,1)
 let endDate = System.DateTime(2016,12,31)
 
+let filterRows (dataFrame : Frame<'a, 'b>) colIndex predicate =
+    let column = dataFrame.GetColumn(colIndex)
+    let trueRowKeys = column.Where(fun value -> predicate value.Value = true).Keys
+    let filteredDataFrame = dataFrame.Rows.[trueRowKeys]
+    filteredDataFrame
+
+let mergeFramesOn (frame1 : Frame<'a, 'b>) (frame2 : Frame<'a, 'b>) frame1ColIndex frame2ColIndex =
+    let frame1Column = frame1.GetColumn(frame1ColIndex)
+    let frame2Column = frame2.GetColumn(frame2ColIndex)
+    let trueFrame1Rows = frame1Column.Where(fun value -> frame2Column.Values |> Seq.contains value.Value).Keys
+    let frame2Cols = frame2.Columns.Keys
+    for key in frame2.Columns.Keys do
+        if key != frame2ColIndex then
+            let column = frame2.GetColumn(key)
+            for i = 0 to i = frame2.RowCount do
+                
+
+        
+        
+
+
 let joistSold =
     let joistSold = Deedle.Frame.ReadCsv(dataPath + @"\Fallon\Jobs Sold.csv")
     joistSold.ReplaceColumn("J. PO Date", joistSold.GetColumn<System.DateTime>("J. PO Date"))
-    let isInDateRange = 
-        joistSold.GetColumn("J. PO Date")
-        |> Series.mapValues (fun date -> date >= startDate && date <= endDate)
-    joistSold.AddColumn("IsInDateRange", isInDateRange)
-    let joistSold = joistSold.FilterRowsBy("IsInDateRange", true)
-    let joistSold = joistSold.FillMissing("BLANK")
+    let isInDateRange value = value >= startDate && value <= endDate
+    let joistSold = filterRows joistSold "J. PO Date" isInDateRange
     joistSold
 
-let joistSoldPODateFilt =
-    let joistSoldDate = joistSold.GetColumn<System.DateTime>("J. PO Date") 
-    let joistSoldJobNumber = joistSold.GetColumn<string>("Job Number")   
-    let frame = frame ["J. PO Date" => joistSoldDate]
-    frame.AddColumn("Job Number", joistSoldJobNumber)
-    frame
+let secondColumn = joistSold.Columns.["This"]
 
 
-    
-
-
-
-
-    
+let joistSoldPODateFilt = joistSold.Columns.[["J. PO Date"; "Job Number"]]
 
 let quotedVsSold = Deedle.Frame.ReadCsv(dataPath + @"\Fallon\Quoted VS Sold.csv")
 
