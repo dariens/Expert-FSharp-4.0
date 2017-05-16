612e8e00466964f651a7da402f75cd251a44c225
dariens <darien.shannon@gmail.com>
2017-05-15 23:28:32 -0700

    started chapter 8

--- Chapter7/Chapter7.fsx
+++ Chapter7/Chapter7.fsx
@@ -73,4 +73,19 @@ module public VisitorCredentials =
         visitorTable.Keys
 
 
+type Vector2D =
+    {DX : float; DY : float}
+
+module Vector2DOps =
+    let length v = sqrt (v.DX * v.DX + v.DY * v.DY)
+    let scale k v = {DX = k * v.DX; DY = k * v.DY}
+    let shiftX x v = {v with DX = v.DX + x}
+    let shiftY y v = {v with DY = v.DY + y}
+    let zero = {DX = 0.0; DY = 0.0}
+    let constX dx = {DX = dx; DY = 0.0}
+    let constY dy = {DX = 0.0; DY = dy}
+
+
+
+
 
